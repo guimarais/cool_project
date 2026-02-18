b43096982c8add7f4b56cbb72ce6c24d84c45507
guimarais <luis.guimarais@gmail.com>
2026-02-18 17:53:26 +0000

    I am plotting geodata now

--- geopandas_test.py
+++ geopandas_test.py
@@ -1,3 +1,9 @@
+# Import necessary libraries
 import geopandas as gpd
 
-print("hello")
+# Load a GeoJSON file containing geospatial data
+url = "https://raw.githubusercontent.com/nmota/caop_GeoJSON/master/ContinenteConcelhos.geojson"
+districts = gpd.read_file(url)
+
+# Plot the geospatial data
+districts.plot()
